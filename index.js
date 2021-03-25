b7317e7952885a214d43adc8ae1f2997c4757c16
Daniel_Serrano <ingm.daniel.serrano@gmail.com>
2021-03-24 20:49:23 -0500

    ajustes para deploy

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use( cors() );
 // habilitar xpress.json
 app.use( express.json({ extended : true }) );
 // puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 // importar rutas
 app.use( '/api/usuarios', require( './routes/users' ) );
@@ -18,6 +18,6 @@ app.use( '/api/auth', require( './routes/auth' ) );
 app.use( '/api/proyectos', require( './routes/proyectos' ) );
 app.use( '/api/tareas', require( './routes/tareas' ) );
 // arrancar la app
-app.listen( PORT, () => {
+app.listen( port, '0.0.0.0', () => {
     console.log( `El servidor esta funcionando en el puerto ${ PORT }` );
 })
